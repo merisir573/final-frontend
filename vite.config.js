ec6ab44cacdbff300768fc87e018b540441a75ac
merisir573 <aikosstory@gmail.com>
2025-01-30 12:27:26 +0300

    update

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    port: process.env.PORT || 3005, // Use Render's assigned port or fallback to 3005
-    host: '0.0.0.0' // Ensures it binds correctly
-  }
-});
